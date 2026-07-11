0c011d8cbd455419b84d77fdf5c30eef506c93a2
naveesh-007 <23501a4255@pvpsit.ac.in>
2026-07-11 17:43:28 +0530

    Added Spring Data JPA and Maven assignment

--- week-2/Spring Core Maven/DependencyInjection/src/main/java/com/example/dependencyinjection/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dependencyinjection.repository;
-
-public class BookRepository {
-
-    public String getBookTitle() {
-        return "Spring in Action";
-    }
-}
--- week-2/Spring Data JPA/LibraryManagement/src/main/resources/ data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO books(id,title,author,price)
-VALUES(1,'Spring Boot','John',499);
-
-INSERT INTO books(id,title,author,price)
-VALUES(2,'Hibernate','David',699);
